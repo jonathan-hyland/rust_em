e8948684cb5f2d9f993a50f0bebccabd85798511
Jonathan Hyland <85582235+jonathan-hyland@users.noreply.github.com>
2021-07-26 15:21:10 -0400

    Refactored if-else into match, added inline comments

--- src/main.rs
+++ src/main.rs
@@ -15,28 +15,42 @@ fn start_app() {
     println!(
         "Please select from the following options:\n
         1 - Employee administration\n
-        2 - Access employee directory"
+        2 - Access employee directory\n
+        3 - Exit"
     );
 
     let mut cmd = String::new();
     let mut start_loop = true;
 
     while start_loop == true {
+
+        // Capture user input as a String, then trim the string.
         io::stdin()
             .read_line(&mut cmd)
             .expect("Please enter a valid command.");
-    
-        if cmd.trim() == "1".to_string() {
-            println!("Accessing employee administration...");
-            start_loop = false;
-            employee_admin::start_administration();
-        } else if cmd.trim() == "2".to_string() {
-            println!("Accessing employee directory...");
-            start_loop = false;
-        } else {
-            println!("Please enter a valid command.");
-            cmd.clear();
-            start_loop = true;
+        
+        let cmd_trim = cmd.trim();
+
+        // Match statement parses the str into an i32 and compares passed value to the arms.
+        match cmd_trim.parse::<i32>() {
+            Ok(1) => {
+                println!("Accessing employee administration...");
+                start_loop = false;
+                employee_admin::start_administration();
+            }
+            Ok(2) => {
+                println!("Accessing employee directory...");
+                start_loop = false;
+            }
+            Ok(3) => {
+                println!("Good bye!");
+                start_loop = false;
+            },
+            _ => {
+                println!("Please enter a valid command.");
+                cmd.clear();
+                start_loop = true;
+            }
         }
     }
 }
